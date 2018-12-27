6e4c135b54eb3cd43abbdbdcaf141f80d105172f
a991817 <981502062@qq.com>
2018-12-27 19:48:24 +0800

    add sql file

---
+++ CLASS.sql
@@ -0,0 +1,53 @@
+--------------------------------------------------------
+--  文件已创建 - 星期四-十二月-27-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table CLASS
+--------------------------------------------------------
+
+  CREATE TABLE "SYSTEM"."CLASS" 
+   (	"CNO" VARCHAR2(20 BYTE), 
+	"CNAME" VARCHAR2(20 BYTE), 
+	"NUMBERS" NUMBER DEFAULT 0
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+REM INSERTING into SYSTEM.CLASS
+SET DEFINE OFF;
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl1','软件工程卓越1班',1);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl2','软件工程卓越2班',4);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl3','计算机科学与技术1班',4);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl4','软件工程1班',4);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl5','软件工程2班',4);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl6','网络工程1班',4);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl9','网络工程2班',0);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl10','信科1班',0);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl11','信科2班',0);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl12','通信工程2班',1);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl13','软件工程3班',0);
+Insert into SYSTEM.CLASS (CNO,CNAME,NUMBERS) values ('cl14','软件工程4班',0);
+--------------------------------------------------------
+--  DDL for Index CLASS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "SYSTEM"."CLASS_PK" ON "SYSTEM"."CLASS" ("CNO") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  Constraints for Table CLASS
+--------------------------------------------------------
+
+  ALTER TABLE "SYSTEM"."CLASS" ADD CONSTRAINT "CLASS_PK" PRIMARY KEY ("CNO")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM"  ENABLE;
+ 
+  ALTER TABLE "SYSTEM"."CLASS" MODIFY ("CNO" NOT NULL ENABLE);
+ 
+  ALTER TABLE "SYSTEM"."CLASS" MODIFY ("CNAME" NOT NULL ENABLE);
+ 
+  ALTER TABLE "SYSTEM"."CLASS" MODIFY ("NUMBERS" NOT NULL ENABLE);
